d91da967de5ef3b037304f793978d01d19df0c70
Bourhanoudine Mamod Houssen <bmamodhousse@emi.u-bordeaux1.fr>
2019-11-21 16:17:12 +0100

    grejgeriojg

--- Dukes of the Realm/src/Struct/Chateau.java
+++ Dukes of the Realm/src/Struct/Chateau.java
@@ -4,19 +4,22 @@ import java.util.ArrayList;
 
 import Troupes.Troupes;
 
-public class Chateau {
+public class Chateau extends Royaume{
 	private int lvl=1;
-	//private Player proprio;
+	private Player proprio;
 	private ArrayList<Troupes> troupe;
 	private int prod;
 	private ArrayList<Troupes> file;
 	private int orientation;
 	private int tresor;
+	private int position_x, position_y;
 	
 	
-	public Chateau(/*player*/){
+	
+	
+	public Chateau(Player proprio){
 		lvl=1;
-		//proprio = player;
+		this.setProprio(proprio);
 		troupe = new ArrayList<Troupes>();
 		prod = 0;
 		file = new ArrayList<Troupes>();
@@ -96,6 +99,33 @@ public class Chateau {
 
 	public void setTresor(int tresor) {
 		this.tresor = tresor;
+	}
+
+	
+	
+	
+	public int getPosition_x() {
+		return position_x;
+	}
+
+	public void setPosition_x(int position_x) {
+		this.position_x = position_x;
+	}
+
+	public int getPosition_y() {
+		return position_y;
+	}
+
+	public void setPosition_y(int position_y) {
+		this.position_y = position_y;
+	}
+
+	public Player getProprio() {
+		return proprio;
+	}
+
+	public void setProprio(Player proprio) {
+		this.proprio = proprio;
 	}
 	
 	/*public void increaseTresor() {
--- Dukes of the Realm/src/Troupes/Troupes.java
+++ Dukes of the Realm/src/Troupes/Troupes.java
@@ -7,7 +7,9 @@ public abstract class Troupes {
 	private int pdv;
 	private int deg;
 	private String name;
-
+	private int position_x, position_y;
+	
+	
 	
 	public int getCout() {
 		return cout;
@@ -37,5 +39,23 @@ public abstract class Troupes {
 		pdv=vie;
 		return;
 	}
+
+	public int getPosition_x() {
+		return position_x;
+	}
+
+	public void setPosition_x(int position_x) {
+		this.position_x = position_x;
+	}
+
+	public int getPosition_y() {
+		return position_y;
+	}
+
+	public void setPosition_y(int position_y) {
+		this.position_y = position_y;
+	}
+	
+
 	
 }
