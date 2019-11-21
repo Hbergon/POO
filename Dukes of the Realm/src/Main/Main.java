9a4ebb53d16f66f0a0819a28b2e12a0c9c30d0ba
Hugo Bergon <hugobergon@hotmail.fr>
2019-11-21 15:49:04 +0100

    ddd

--- Dukes of the Realm/src/Main/Main.java
+++ Dukes of the Realm/src/Main/Main.java
@@ -1,5 +1,8 @@
 package Main;
 import Troupes.*;
+
+import java.util.ArrayList;
+
 import Struct.*;
 
 public class Main {
@@ -10,6 +13,28 @@ public class Main {
 		
 		Chevalier test2 = new Chevalier();
 		System.out.println(test2.getPdv());
+		
+		Onagre test3 = new Onagre();
+		System.out.println(test3.getPdv());
+		
+		Chateau testC = new Chateau();
+		System.out.println(testC.getProd());
+		
+		
+		testC.addToTroupe(test);
+		testC.addToTroupe(test2);
+		System.out.println(testC.getTroupe());
+		
+		testC.addToFile(test3);
+		System.out.println(testC.getList());
+		testC.removeFirstList();
+		System.out.println(testC.getList());
+		
+		/*ArrayList<Troupes> array = new ArrayList<Troupes>();
+		array.add(test);
+		array.add(test2);*/
+		
+
 		
 	}
 
--- Dukes of the Realm/src/Struct/Chateau.java
+++ Dukes of the Realm/src/Struct/Chateau.java
@@ -1,5 +1,104 @@
 package Struct;
 
-public class Chateau {
+import java.util.ArrayList;
 
+import Troupes.Troupes;
+
+public class Chateau {
+	private int lvl=1;
+	//private Player proprio;
+	private ArrayList<Troupes> troupe;
+	private int prod;
+	private ArrayList<Troupes> file;
+	private int orientation;
+	private int tresor;
+	
+	
+	public Chateau(/*player*/){
+		lvl=1;
+		//proprio = player;
+		troupe = new ArrayList<Troupes>();
+		prod = 0;
+		file = new ArrayList<Troupes>();
+		setTresor(0);
+	}
+	
+	public int getLvl() {
+		return lvl;
+	}
+	
+	/*public Player getPlayer() {
+		return proprio;
+	}*/
+
+	public Troupes getSomeTroupe(int ind) {
+		return troupe.get(ind);
+	}
+	
+	public ArrayList<Troupes> getTroupe() {
+		return troupe;
+	}
+	
+	public void removeFirstTroupe() {
+		troupe.remove(0);
+		return ;
+	}
+	
+	public void addToTroupe(Troupes tr) {
+		troupe.add(tr);
+		return;
+	}
+	
+	public ArrayList<Troupes> getList() {
+		return file;
+	}
+	
+	public Troupes getFirstList() {
+		 return file.get(0);
+	}
+	
+	public void removeFirstList() {
+		file.remove(0);
+		return ;
+	}
+	
+	
+	public int getProd() {
+		return prod;
+	}
+
+	public void setLvl(int lvl) {
+		this.lvl=lvl;
+		return;
+	}
+	
+	public void setProd(int cout) {
+		prod=cout;
+		return;
+	}
+	
+	public void addToFile(Troupes tr) {
+		file.add(tr);
+		return;
+	}
+
+	public int getOrientation() {
+		return orientation;
+	}
+
+	public void setOrientation(int orientation) {
+		this.orientation = orientation;
+	}
+
+	public int getTresor() {
+		return tresor;
+	}
+
+	public void setTresor(int tresor) {
+		this.tresor = tresor;
+	}
+	
+	/*public void increaseTresor() {
+		
+	}*/
 }
