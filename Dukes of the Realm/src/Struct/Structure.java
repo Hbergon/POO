34b321c568edf68a178b4210da5043c92fe308c9
Hugo Bergon <hugobergon@hotmail.fr>
2019-11-21 16:46:27 +0100

    truc

--- Dukes of the Realm/src/Struct/Structure.java
+++ Dukes of the Realm/src/Struct/Structure.java
@@ -5,7 +5,7 @@ import Troupes.*;
 
 public abstract class Structure {
 	private int lvl;
-	//private Player proprio;
+	private Player proprio;
 	private Troupes troupe;
 	private int prod;
 	private ArrayList<Troupes> file;
@@ -14,27 +14,14 @@ public abstract class Structure {
 		return lvl;
 	}
 	
-	/*public Player getPlayer() {
+	public Player getPlayer() {
 		return proprio;
-	}*/
+	}
 
-	public Troupes gettroupe() {
-		return troupe;
-	}
-	
-	public ArrayList<Troupes> getList() {
-		return file;
-	}
 
 	public void setPdv(int lvl) {
 		this.lvl=lvl;
 		return;
 	}
-	
-	public void setProd(int cout) {
-		prod=cout;
-		return;
-	}
-	
 	
 }
